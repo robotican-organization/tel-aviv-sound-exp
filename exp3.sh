7282426ab23e49755bd21bdc4bf812b51629c157
elhayrobotican <elhay@robotican.net>
2017-08-17 15:15:50 +0300

    bug fixes

--- exp3.sh
+++ exp3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TIME_TO_SLEEP=3
+TIME_TO_DRIVE=5
 
 echo "Start Driving..."
-rostopic pub -1 /turtle1/cmd_vel geometry_msgs/Twist -- '[1.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]' &
+rostopic pub -r 1 /cmd_vel geometry_msgs/Twist -- '[1.0, 0.0, 0.0]' '[0.0, 0.0, 0.0]' &
 PID=$!
 ./exp1.sh &
-sleep(TIME_TO_SLEEP)
+sleep $TIME_TO_DRIVE
 echo "Stop Driving..."
 kill $PID
